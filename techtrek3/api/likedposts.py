87f76e13932a56c680664950bb47922190a0d9b4
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2022-01-20 15:06:53 +0800

    Delete likedposts.py

--- techtrek3/api/likedposts.py
+++
@@ -1,4 +0,0 @@
-likedposts = db.Table('likedposts',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'), primary_key=True),
-    db.Column('post_is', db.Integer, db.ForeignKey('posts.post_id'), primary_key=True)
-)
