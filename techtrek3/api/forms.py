d22986d258fd1d9cdc33882102cb6a7a8e5758f0
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2022-01-20 15:08:15 +0800

    Create forms.py

---
+++ techtrek3/api/forms.py
@@ -0,0 +1,48 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email, ValidationError
+from user import User
+
+
+
+def user_exists(form, field):
+    # Checking if user exists
+    email = field.data
+    user = User.query.filter(User.email == email).first()
+    if user:
+        raise ValidationError('Email address is already in use.')
+
+
+def username_exists(form, field):
+    # Checking if username is already in use
+    username = field.data
+    user = User.query.filter(User.username == username).first()
+    if user:
+        raise ValidationError('Username is already in use.')
+
+def password_matches(form, field):
+    # Checking if password matches
+    password = field.data
+    email = form.data['email']
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError('No such user exists.')
+    if not user.check_password(password):
+        raise ValidationError('Password was incorrect.')
+
+class SignUpForm(FlaskForm):
+    username = StringField(
+        'username', validators=[DataRequired(), username_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists])
+    password = StringField('password', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), user_exists])
+    password = StringField('password', validators=[
+                           DataRequired(), password_matches])
+
+
+class PostForm(Form):
+    title = TextAreaField("Post Title", validators=[DataRequired()])
+	content = TextAreaField("Post Desc", validators = [DataRequired()])
