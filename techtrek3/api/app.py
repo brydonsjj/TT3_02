d87eba5795c781f80ca197e062186ddb4a76b680
Brydon Seah <brydonseah@gmail.com>
2022-01-20 15:12:47 +0800

    Updated app.py to SQL

--- techtrek3/api/app.py
+++ techtrek3/api/app.py
@@ -1,105 +1,45 @@
-from flask import Flask
-
-from flask import (Flask, g, render_template, flash, redirect, url_for,abort)
-from flask_bcrypt import check_password_hash
-from flask_login import (LoginManager, login_user, logout_user,
-							login_required, current_user)
-
-from user import User
-from signup_form import SignUpForm
-from login_form import LoginForm
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.debug = True
-app.secret_key = ''
-"""here secret_key is a random string of alphanumerics"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/socialmedia'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy(app)
+CORS(app)
 
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
-    return errorMessages
+class User(db.Model):
+  
+    tablename = 'user'
+    user_ID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    birthday = db.Column(db.DateTime, nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    phone = db.Column(db.String(64), nullable=False)
+    city = db.Column(db.String(64), nullable=False)
+    country = db.Column(db.String(64), nullable=False)
 
-@app.before_request
-def before_request():
-	"""Connect to database before each request
-		g is a global object, passed around all time in flask, used to setup things which
-		we wanna have available everywhere.
-	"""
-    client = pymongo.MongoClient("")
-    g.db = client['DBSDatabase']
-	g.db.connect()
-	g.user = current_user
+    def init(self, user_ID, name, age, birthday, email, phone, city, country): #Initialise the objects
+        self.user_ID = user_ID
+        self.name = name
+        self.age = age
+        self.birthday = birthday
+        self.email = email
+        self.phone = phone
+        self.city = city
+        self.country = country
+        
 
-@app.after_request
-def after_request(response):
-	"""close all database connection after each request"""
-	g.db.close()
-	return response
+    def json(self):
+        return {"user_ID": self.user_ID, "name": self.name,  "age": self.age, "birthday": self.birthday, 
+            "email": self.email, "phone": self.phone, "city": self.city, "country": self.city}
 
 
-@auth_routes.route('/')
-def authenticate():
-    """
-    Authenticates a user.
-    """
-    if current_user.is_authenticated:
-        return current_user.to_dict()
-    return {'errors': ['Unauthorized']}
-
-
-
-@auth_routes.route('/signup', methods=['POST'])
-def sign_up():
-    #Creates a new user and logs them in
-	form = SignUpForm()
-	if form.validate_on_submit():
-        user = User(
-            username=form.data['username'],
-            email=form.data['email'],
-            password=form.data['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        return user.to_dict()
-	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
-
-@app.route('/login', methods = ('GET', 'POST'))
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter(User.email == form.data['email']).first()
-		login_user(user)
-		return user.to_dict()
-	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
-
-@app.route('/logout')
-@login_required
-def logout():
-	logout_user()
-	return {'message': 'User logged out'}
-
-@auth_routes.route('/unauthorized')
-def unauthorized():
-    """
-    Returns unauthorized JSON when flask-login authentication fails
-    """
-    return {'errors': ['Unauthorized']}, 401
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/', methods=['GET'])
+def get_all_users():
+    return jsonify({"users": [user.json() for user in User.query.all()]})
+    
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0',port=5010, debug=True)
