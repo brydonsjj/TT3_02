49c4e5ab26e7fde0b705ef6b2f73e34f0ada158c
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2022-01-20 13:57:55 +0800

    added login and signup

--- techtrek3/api/app.py
+++ techtrek3/api/app.py
@@ -6,6 +6,8 @@ from flask_login import (LoginManager, login_user, logout_user,
 							login_required, current_user)
 
 from user import User
+from signup_form import SignUpForm
+from login_form import LoginForm
 
 app = Flask(__name__)
 app.debug = True
@@ -44,8 +46,60 @@ def after_request(response):
 	return response
 
 
-@app.route('/')
-def home():
-    return {"message": "hello there my name"}
+@auth_routes.route('/')
+def authenticate():
+    """
+    Authenticates a user.
+    """
+    if current_user.is_authenticated:
+        return current_user.to_dict()
+    return {'errors': ['Unauthorized']}
+
+
+
+@auth_routes.route('/signup', methods=['POST'])
+def sign_up():
+    #Creates a new user and logs them in
+	form = SignUpForm()
+	if form.validate_on_submit():
+        user = User(
+            username=form.data['username'],
+            email=form.data['email'],
+            password=form.data['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return user.to_dict()
+	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@app.route('/login', methods = ('GET', 'POST'))
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter(User.email == form.data['email']).first()
+		login_user(user)
+		return user.to_dict()
+	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return {'message': 'User logged out'}
+
+@auth_routes.route('/unauthorized')
+def unauthorized():
+    """
+    Returns unauthorized JSON when flask-login authentication fails
+    """
+    return {'errors': ['Unauthorized']}, 401
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
